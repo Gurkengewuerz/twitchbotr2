22755770020e611ebced1f2ae9484efab4939863
Niklas <niklas@schuetrumpf.net>
2016-12-21 23:07:39 +0100

    started implementing TwitchChatAPI

---
+++ src/main/java/de/gurkengewuerz/twitchbotr2/listener/MessageListener.java
@@ -0,0 +1,30 @@
+package de.gurkengewuerz.twitchbotr2.listener;
+
+import de.gurkengewuerz.twitchbotr2.TwitchBotR2;
+import de.lukweb.twitchchat.events.Handler;
+import de.lukweb.twitchchat.events.Listener;
+import de.lukweb.twitchchat.events.irc.IrcReceiveMessageEvent;
+import de.lukweb.twitchchat.events.user.UserSendMessageEvent;
+import de.lukweb.twitchchat.events.user.UserSendWhisperEvent;
+
+/**
+ * Created by gurkengewuerz.de on 21.12.2016.
+ */
+public class MessageListener extends Listener {
+
+    @Handler
+    public void onMessage(UserSendMessageEvent e) {
+
+    }
+
+    @Handler
+    public void onWhisper(UserSendWhisperEvent e) {
+
+    }
+
+    @Handler
+    public void onIRCMessage(IrcReceiveMessageEvent e) {
+        // TwitchBotR2.getDashboardInstance().addChatMessage(""); // TODO: Replace at API Update with Message Object
+        System.out.println(e.getMessage());
+    }
+}
